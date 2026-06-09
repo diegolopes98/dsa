d41a97d50feb0012853e814e80095937ceca95c3
Diego Pereira Lopes <diegopereiralopes98@gmail.com>
2026-06-09 08:24:09 -0300

    refactor: bubble sort

--- algorithms/sorting/bubble_sort.py
+++ algorithms/sorting/bubble_sort.py
@@ -4,9 +4,9 @@ from typing import List
 def bubble_sort(numbers: List[int | float]) -> None:
     size = len(numbers)
 
-    for _ in numbers:
+    for k in range(size):
         is_sorted = True
-        for i in range(size - 1):
+        for i in range(size - 1 - k):
             if numbers[i] > numbers[i + 1]:
                 is_sorted = False
                 numbers[i], numbers[i + 1] = numbers[i + 1], numbers[i]
